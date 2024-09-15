a933b38fd63938a637367011415f0e84dd70c98d
Eduardo Viero <duduviero@gmail.com>
2024-09-15 19:46:43 -0300

    getPostBySlug

--- services/api.js
+++ services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
-    headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
-    }
-})
+export const api = axios.create({
+  baseURL: 'https://hidsdhudfjgrdylvfdlp.supabase.co/rest/v1/',
+  headers: {
+    apikey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpZHNkaHVkZmpncmR5bHZmZGxwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjQzNzc0NSwiZXhwIjoyMDQyMDEzNzQ1fQ.aRnLzPu21ffsRM9-DI_so6M3Qf7UtcJaZX_vFeH51Y0',
+    authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpZHNkaHVkZmpncmR5bHZmZGxwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjQzNzc0NSwiZXhwIjoyMDQyMDEzNzQ1fQ.aRnLzPu21ffsRM9-DI_so6M3Qf7UtcJaZX_vFeH51Y0',
+  },
+});
--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -1,19 +1,23 @@
-import { api } from '../services/api'
+import { api } from '../services/api';
 
 export const getPosts = async () => {
-    const {data} = await api.get('/posts'); 
+  const { data } = await api.get('/posts?select=*');
 
-    if(data){
-        return data;
-    }
+  if (data) {
+    return data;
+  }
 
-    return []
-}
+  return [];
+};
 
 export const getPostBySlug = async (id) => {
+  //TODO: BUSCAR UM POST EM ESPECIFICO.
+  const { data } = await api.get(`posts?id=eq.${id}&select=*`);
+  console.log(data);
 
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
+  if (data) {
+    return data[0];
+  }
 
-    return {}
-}
+  return {};
+};

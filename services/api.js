47a55b31f720d8569ee49032a9eaf86a24fedc09
Eduardo Viero <duduviero@gmail.com>
2024-09-15 19:54:54 -0300

    getPostBySlug

--- services/api.js
+++ services/api.js
@@ -3,9 +3,7 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://hidsdhudfjgrdylvfdlp.supabase.co/rest/v1/',
   headers: {
-    apikey:
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpZHNkaHVkZmpncmR5bHZmZGxwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjQzNzc0NSwiZXhwIjoyMDQyMDEzNzQ1fQ.aRnLzPu21ffsRM9-DI_so6M3Qf7UtcJaZX_vFeH51Y0',
-    authorization:
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpZHNkaHVkZmpncmR5bHZmZGxwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjQzNzc0NSwiZXhwIjoyMDQyMDEzNzQ1fQ.aRnLzPu21ffsRM9-DI_so6M3Qf7UtcJaZX_vFeH51Y0',
+    apikey: `${process.env.SUPABASE_ANON_KEY}`,
+    authorization: `Bearer ${process.env.SUPABASE_ANON_TOKEN}`,
   },
 });
